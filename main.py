24611fe32af428a8be4969d0f066dd03a4e95326
olivier-truong-esiee <olivier.truong@edu.esiee.fr>
2025-12-04 10:03:07 +0100

    Improve isprime function with documentation
    Refactor isprime function and add docstring.

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-from math import sqrt
+# main.py
+"""
+Nombres premiers
+"""
 
-#### Fonction secondaire
+def isprime(p: int) -> bool:
+    """
+    isprime(p : int) -> bool
+    Args:
+        p: int
+
+    Returns:
+        bool
+    
+    >>> isprime(7)
+    True
+    >>> isprime(10)
+    False
+    """
+
+    premier = True
+    borne_max = int(p**0.5) + 1
+    for i in range(2, borne_max):
+        if p % i == 0:
+            premier = False
+            break
+    return premier
 
 
-def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Appel principal
+    """
     for n in range(100):
         if isprime(n):
-            print(n, end=", ")
+            print(n, end=', ')
 
     print()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
